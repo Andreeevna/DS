1da39ba442df1d213628defff01167c6bbab9add
DankAvHi <thexcat86@gmail.com>
2024-03-21 22:31:30 +0300

    final fix i wish

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
 	},
 	server: {
 		proxy: {
-		  "/api": {
-			target: "http://localhost:5000",
+		  '/api': {
+			target: 'http://localhost:5000',
 			changeOrigin: true,
-			secure: false,
+			rewrite: (path) => path.replace(/^\/api/, ''),
 		  },
 		},
 	  },
